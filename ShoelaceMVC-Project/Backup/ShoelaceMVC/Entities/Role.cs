a01475a25af576e6f08a96debd7bf90499efc15a
Ryan Hayes <ryanhayes84@gmail.com>
2012-07-22 23:29:49 -0400

    Upgraded to VS2012.

---
+++ ShoelaceMVC-Project/Backup/ShoelaceMVC/Entities/Role.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace ShoelaceMVC.Entities
+{
+    public class Role
+    {
+        //Membership required
+        [Key()]
+        public virtual Guid RoleId { get; set; }
+        [Required()]
+        [MaxLength(100)]
+        public virtual string RoleName { get; set; }
+
+        public virtual ICollection<User> Users { get; set; }
+
+        //Optional
+        [MaxLength(250)]
+        public virtual string Description { get; set; }
+    }
+}
